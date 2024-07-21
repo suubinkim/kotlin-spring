2140c83e1e4c5faff5835a283abfe2b335b8972d
suubinkim <0vbbbv0@gmail.com>
2024-07-21 21:42:15 +0900

    kakao api 사용

--- src/main/kotlin/com/example/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/example/study/blog/service/BlogService.kt
@@ -1,11 +1,40 @@
 package com.example.study.blog.service
 
 import com.example.study.blog.dto.BlogDto
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
 class BlogService {
+    @Value("\${REST_API_KEY}")
+    lateinit var restApiKey : String
+
     fun searchKakao(blogDto: BlogDto): String? {
-        return "SearchKakao"
+        val webClient = WebClient
+            .builder()
+            .baseUrl("https://dapi.kakao.com")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build()
+
+        val response = webClient
+            .get()
+            .uri {
+                it.path("/v2/search/blog")
+                    .queryParam("query", blogDto.query)
+                    .queryParam("sort", blogDto.sort)
+                    .queryParam("page", blogDto.page)
+                    .queryParam("size", blogDto.size)
+                    .build()
+            }
+            .header("Authorization", "KakaoAK $restApiKey")
+            .retrieve()
+            .bodyToMono<String>()
+        val result = response.block()
+
+        return result
     }
 }
